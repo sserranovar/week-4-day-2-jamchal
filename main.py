a0ac18e0b2ee7aecc31611792ab763a33ab173cd
sserranovar <125494152+sserranovar@users.noreply.github.com>
2023-09-13 20:00:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+Name=input("What is your name? ")
+Totalsales=float(input("How much have you sold?"))                                     
+comissions=( Totalsales * 13 / 100)
+print(f"Congratulations, {Name}, you have earned ${comissions} in comissions!")
